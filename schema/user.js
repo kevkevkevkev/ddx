d3c55f426ba79b7ca2b7d360b04e5b78165c2f2a
kevkevkevkev <kevino@stanford.edu>
2017-04-09 22:47:29 -0700

    Added loadDatabase.js to package.json

--- schema/user.js
+++ schema/user.js
@@ -9,10 +9,15 @@ var mongoose = require('mongoose');
 // create a schema
 var userSchema = new mongoose.Schema({
     id: String,     // Unique ID identifying this user
-    email_address: String, // Email address/login for this user
-    password: String,   // The password of the user
     first_name: String, // First name of the user.
     last_name: String,  // Last name of the user.
+    location: String,    // Location  of the user.
+    description: String,  // A brief user description
+    occupation: String,    // Occupation of the user.
+    login_name: String,	// The login_name of the user.
+    password: String,	// The password of the user
+    photos_liked: [mongoose.Schema.Types.ObjectId], // An array of IDs of the photos liked by the user
+    photos_favorited: [mongoose.Schema.Types.ObjectId] // An array of the IDs of the photos favorited by the user
 });
 
 // the schema is useless so far
